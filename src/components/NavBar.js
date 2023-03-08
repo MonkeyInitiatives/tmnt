1f1da26558eb31a6f0f8b552463c0ba1708b42a8
Thomas Woods <>
2023-03-07 19:43:20 -0500

    fixed navbar home linking

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@
 function NavBar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand" href="/" onClick={() => props.handlePageChange('Home')}>Home</a>
+            <a className="navbar-brand" href="#home" onClick={() => props.handlePageChange('Home')}>Home</a>
             {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button> */}
